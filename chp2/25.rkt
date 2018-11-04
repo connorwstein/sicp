80eb6dd5bb524230d895387a883b2b16a92d18aa
connorwstein <connor.stein@mail.mcgill.ca>
2018-11-04 15:16:57 -0500

    just reading 2.5, not particularly interesting

---
+++ chp2/25.rkt
@@ -0,0 +1,14 @@
+; Exercise 2.77
+; magnitude z 
+;   real-part z and imag-part z
+;   each real-part and imag-part gets called twice, first to extract the complex
+;   then to extract the rect/polar tag 
+
+; Exercise 2.79
+; ordinary numbers just use equal? directly
+; rational numbers need to compare both numerator and denominator
+; complex compare real and imag parts
+
+
+
+
