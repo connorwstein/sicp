2dc02f2b8fb19f30b8103342da3521e6377b070b
connorwstein <connor.stein@mail.mcgill.ca>
2019-01-06 20:19:02 -0500

    multiple-dwelling

--- chp4/43.rkt
+++ chp4/43.rkt
@@ -20,3 +20,49 @@
       (let ((i (an-integer-between 1 j)))
         (require (= (+ (* i i) (* j j)) (* k k)))
         (list i j k)))))
+
+
+; Exercise 4.38
+; With the original contraints, you can see there is only one solution by evaluating where fletcher (F) can live (good 
+; place to start because he has the most constraints on him) then looking at the cascading effects
+; F on 5 --> not allowed directly by constraint
+; F on 1 --> not allowed directly by constraint
+; F on 2 --> forces S and C to be on 4,5 but that would be M > C 
+; F on 3 --> forces S and C to be on 5,1 but C != 1 and C = 5 would break M > C
+; F on 4 --> only one that works, forces S and C to 2,1 (C != 1, so C = 2 and S = 1), that leave B=3 (can't be 5) and M=5
+; Now if we remove the constraint that F and S can't be adjacent, that opens up some new possibilities. 
+; F = 3 still doesn't work, because C is forced to 5 which breaks M > C. F = 2 is now possible with M = 5 and C = 2  and 2 permutations
+; for B and S, so we gained two solutions there. The original F = 4 also has new solutions - C is forced to 2 then, in addition to the original
+; solution we can have S=5 F=4 M=3 C=2 B=1 and M=5 F=4 S=3 C=2 B=1. So total now is 5 solutions.
+
+
+; This is cool its a little grammar parser
+; word-list is a list starting with the type followed by instances of the type
+; ex: '('noun 'cat 'dog ...)
+; Then we walk through *unparsed* checking whether the next unparsed word is in the word-list, if so return 
+; '(word-type word) 
+(define (parse-word word-list)
+  (require (not (null? *unparsed*)))
+  (require (memq (car *unparsed*) (cdr word-list)))
+  (let ((found-word (car *unparsed*)))
+    (set! *unparsed* (cdr *unparsed*))
+    (list (car word-list) found-word)))
+
+(define *unparsed* '())
+(define (parse input)
+  (set! *unparsed* input)
+  (let ((sent (parse-sentence)))
+    (require (null? *unparsed*))
+    sent))
+
+; Exercise 4.49 
+; Sentence generation
+
+
+
+
+
+
+
+
+
